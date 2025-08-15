c165df2b71d504eb0f596e27afc2426ff1b08099
Nathaniel Gerlek <ngerlek@inferlink.com>
2025-08-14 20:59:32 -0700

    fixup: data

--- src/module1/mod1_a.py
+++ src/module1/mod1_a.py
@@ -1,13 +1,14 @@
-from pathlib import Path
+from module1 import DATA_DIR
 from module1.mod1_b import mod1_b_func
 from module2.mod2_a import mod2_a_func
 
 
 def mod1_a_func(string: str) -> str:
-    with open("data/module1/mod_1_file.txt", "r") as f:
+    with DATA_DIR.open("r") as f:
         data1 = f.read().strip()
     with open("data/others/other_file.txt", "r") as f:
         data2 = f.read().strip()
+
     return (f"This is mod1_a_func('{string}'), which calls {mod1_b_func(string)}.\n"
             f"It also calls the other module: {mod2_a_func(string)}\n"
             f"Module 1 also can access data, such as \"{data1}\" and \"{data2}\"")
