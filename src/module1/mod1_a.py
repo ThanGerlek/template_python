3fb02d2d4d7440a26ba3946e97f4757271beec01
Nathaniel Gerlek <ngerlek@inferlink.com>
2025-08-14 20:24:27 -0700

    feat: add data/

--- src/module1/mod1_a.py
+++ src/module1/mod1_a.py
@@ -1,6 +1,16 @@
+from pathlib import Path
 from module1.mod1_b import mod1_b_func
 from module2.mod2_a import mod2_a_func
 
 
 def mod1_a_func(string: str) -> str:
-    return f"mod1_a_func('{string}'), which calls {mod1_b_func(string)}. It also calls the other module: {mod2_a_func(string)}"
+    p = Path("data/module1")
+    print(p.absolute().as_posix())
+    print(p.exists())
+    with open("data/module1/mod_1_file.txt", "r") as f:
+        data1 = f.read().strip()
+    with open("data/others/other_file.txt", "r") as f:
+        data2 = f.read().strip()
+    return (f"This is mod1_a_func('{string}'), which calls {mod1_b_func(string)}.\n"
+            f"It also calls the other module: {mod2_a_func(string)}\n"
+            f"Module 1 also can access data, such as \"{data1}\" and \"{data2}\"")
